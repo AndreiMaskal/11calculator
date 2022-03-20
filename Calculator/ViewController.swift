3edc87ee089938347c495e779aa8aab8dff0d237
AndreiMaskal <101343719+AndreiMaskal@users.noreply.github.com>
2022-03-20 21:15:15 +0100

    All fixed

--- Calculator/ViewController.swift
+++ Calculator/ViewController.swift
@@ -1,17 +1,17 @@
-//
+
 //  ViewController.swift
 //  Calculator
 //
 //  Created by Andrei Maskal on 10/03/2022.
-//
 
 import UIKit
 
 class ViewController: UIViewController  {
     var numberSkrini: Float = 0
     var firstNum: Float = 0
-    var mustSing: Bool = false
+    var mustSing: Bool = true
     var operation: Int = 0
+    var firstEkran = false
     var croup = false
     var currentInput: Float {
         get {
@@ -25,48 +25,66 @@ class ViewController: UIViewController  {
             } else {
                 result.text = "\(newValue)"
             }
-        mustSing = false
+            mustSing = false
         }
     }
     @IBOutlet weak var result: UILabel!
     
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        result.text = "0"
+    }
+    
     @IBAction func number(_ sender: UIButton) {
-        if mustSing  {
+        if mustSing {
             result.text = String(sender.tag)
             mustSing = false
             croup = false
+            firstEkran = true
         } else {
             result.text = result.text! + String(sender.tag)
+            firstEkran = false
+        }
+        if firstEkran && sender.tag == 0 {
+            firstEkran = false
+            result.text = "0"
+            mustSing = true
         }
         numberSkrini = currentInput
     }
+    
     @IBAction func croup(_ sender: UIButton) {
-        if !mustSing && !croup {
+        if !result.text!.contains(".") {
             result.text = result.text! + "."
-            croup = true
+            mustSing = false
         }
     }
+    
     @IBAction func buttons(_ sender: UIButton) {
         if result.text != "" && sender.tag != 16  && sender.tag != 11 {
             firstNum = currentInput
+            
             if sender.tag  == 12 { // plus
                 result.text = "+"
             } else if  sender.tag == 13 { // minus
                 result.text = "-"
-            } else if  sender.tag == 14 { // divide
-                result.text = "/"
-            } else if  sender.tag == 15 { // multiplay
+            } else if  sender.tag == 14 { // multiplay
                 result.text = "*"
+            } else if  sender.tag == 15 { // divide
+                result.text = "/"
             }
         } else if sender.tag == 16 { // sum
             if operation == 12 {
-                result.text = String(firstNum + numberSkrini)
+                currentInput = firstNum + numberSkrini
             } else if operation == 13 {
-                result.text = String(firstNum - numberSkrini)
+                currentInput = firstNum - numberSkrini
             } else if operation == 14 {
-                result.text = String(firstNum / numberSkrini)
+                currentInput = firstNum * numberSkrini
             } else if operation == 15 {
-                result.text = String(firstNum * numberSkrini)
+                currentInput = firstNum / numberSkrini
+                if numberSkrini == 0 {
+                    result.text = "error"
+                }
             }
         } else if sender.tag == 11 {
             result.text = " "
@@ -75,9 +93,10 @@ class ViewController: UIViewController  {
             operation = 0
             result.text = (result.text ?? "") + "0"
         }
-        
         mustSing = true
         croup = true
         operation = sender.tag
+        
     }
 }
+
